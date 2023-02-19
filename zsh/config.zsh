5bdbf63a47e2b3df4a42847dc5b1162bb15abe2e
Michael Schwieger <git@zekurio.xyz>
2023-02-19 01:32:27 +0100

    new hosts added

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -15,4 +15,14 @@ bindkey '^H' backward-kill-word
 #autoload -U compinit; compinit
 
 # set history size to 2^16 and other options
-export HISTSIZE=65536;
+export HISTSIZE=65536
+export SAVEHIST=65536
+
+# we set the history already in the zshrc file
+# so we can just set other options here
+setopt extended_history
+setopt inc_append_history
+setopt share_history
+setopt hist_expire_dups_first
+setopt hist_ignore_dups
+setopt hist_ignore_all_dups
