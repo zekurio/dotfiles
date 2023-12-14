27e4358d4fee3bb4d6f6bc776e1d0376bfca3996
Michael Schwieger <git@zekurio.xyz>
2023-12-14 10:48:59 +0100

    fixed some things, changed some things

--- config/hypr/scripts/idle.sh
+++ config/hypr/scripts/idle.sh
@@ -1,5 +1,4 @@
-swayidle -w timeout 30 '/home/michael/dev/dotfiles/config/hypr/scripts/lock.sh' \
-            timeout 60 'hyprctl dispatch dpms off' \
+swayidle -w timeout 300 '/home/michael/dev/dotfiles/config/hypr/scripts/lock.sh' \
+            timeout 600 'hyprctl dispatch dpms off' \
             resume 'hyprctl dispatch dpms on' \
-            timeout 90 'systemctl suspend' \
             before-sleep '/home/michael/dev/dotfiles/config/hypr/scripts/lock.sh' &
---
+++ zsh/plugins/zsh_plugins.zsh
@@ -0,0 +1,13 @@
+fpath+=( /home/michael/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting )
+source /home/michael/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
+fpath+=( /home/michael/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions )
+source /home/michael/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
+fpath+=( /home/michael/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search )
+source /home/michael/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search/zsh-history-substring-search.plugin.zsh
+fpath+=( /home/michael/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions )
+source /home/michael/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions/zsh-completions.plugin.zsh
+fpath+=( /home/michael/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-romkatv-SLASH-powerlevel10k )
+source /home/michael/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-romkatv-SLASH-powerlevel10k/powerlevel10k.zsh-theme
+source /home/michael/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-romkatv-SLASH-powerlevel10k/powerlevel9k.zsh-theme
+fpath+=( /home/michael/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-birdhackor-SLASH-zsh-exa-ls-plugin )
+source /home/michael/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-birdhackor-SLASH-zsh-exa-ls-plugin/zsh-exa-ls-plugin.plugin.zsh
