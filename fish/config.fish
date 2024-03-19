51fea7c11c3626087f89b118baf7ea17aa1cf88c
Michael Schwieger <git@zekurio.xyz>
2024-03-19 08:48:27 +0100

    switching to fish

---
+++ fish/config.fish
@@ -0,0 +1,10 @@
+# disable greeting
+set -g fish_greeting
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# set starship env var and load starship
+set -x STARSHIP_CONFIG ~/.config/starship/starship.toml
+starship init fish | source
