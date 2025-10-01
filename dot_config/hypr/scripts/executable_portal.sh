a723e039ed6f8524c86232e0f6f7bd2ddcbd4a37
Michael Schwieger <git@zekurio.xyz>
2025-10-01 22:28:07 +0200

    more cleanup

--- dot_config/hypr/scripts/executable_portal.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-/usr/lib/xdg-desktop-portal-hyprland &
-sleep 2
-/usr/lib/xdg-desktop-portal &
--- dot_config/rofi/scripts/emoji/executable_emoji.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-rofi -modi emoji -show emoji -emoji-mode copy -theme ~/.config/rofi/scripts/emoji/style.rasi -emoji-format '{emoji} <span weight="bold">{name}</span> [<span size="small">({group})</span>]'
